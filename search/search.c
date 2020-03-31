e92776c55510f427aad8a40aae1682935ef9a5ee
dudaodong <lanliddd.2007@163.com>
2020-03-31 15:56:28 +0800

    update search function

--- search/search.c
+++ search/search.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <time.h>
 #include "search.h"
 
@@ -17,6 +18,7 @@ int binary_search(int a[], int n, int value)
 
     while (low <= high) {
         //【low, high]取中
+//        int mid = (high - low) / 2;
         int mid = low + ((high - low) >> 1);
 
         if (a[mid] == value) {
@@ -29,6 +31,143 @@ int binary_search(int a[], int n, int value)
     }
     return -1;
 }
+
+/**
+ * 查找第一个值等于给定值的元素
+ * @param a
+ * @param n
+ * @param value
+ * @return
+ */
+int binary_search_equal_first(int a[], int n, int value)
+{
+    int low = 0;
+    int high = n - 1;
+
+    while (low <= high) {
+        int mid = low + ((high - low) >> 1);
+
+        if (a[mid] < value) {
+            low = mid + 1;
+        } else if (a[mid] > value) {
+            high = mid - 1;
+        } else {
+            if ((mid == 0) || (a[mid - 1] != value)) return mid;
+            else high = mid - 1;
+        }
+    }
+    return -1;
+}
+
+/**
+ * 查找最后一个值等于给定值的元素
+ * @param a
+ * @param n
+ * @param value
+ * @return
+ */
+int binary_search_equal_last(int a[], int n, int value)
+{
+    int low = 0;
+    int high = n - 1;
+
+    while (low <= high) {
+        int mid = low + ((high - low) >> 1);
+
+        if (a[mid] < value) {
+            low = mid + 1;
+        } else if (a[mid] > value) {
+            high = mid - 1;
+        } else {
+            if ((mid == n -1) || (a[mid + 1] != value)) return mid;
+            else low = mid + 1;
+        }
+    }
+    return -1;
+}
+
+/**
+ * 查找第一个大于等于给定值的元素
+ * @param a
+ * @param n
+ * @param value
+ * @return
+ */
+int binary_search_gt_first(int a[], int n, int value)
+{
+    int low = 0;
+    int high = n - 1;
+
+    while (low <= high) {
+        int mid = low + ((high - low) >> 1);
+
+        if (a[mid] >= value) {
+            if((mid == 0) || (a[mid-1] < value)) return mid;
+            else high = mid + 1;
+        }else {
+             low = mid + 1;
+        }
+    }
+    return -1;
+}
+
+/**
+ * 查找最后一个小于等于给定值的元素
+ * @param a
+ * @param n
+ * @param value
+ * @return
+ */
+int binary_search_lt_first(int a[], int n, int value)
+{
+    int low = 0;
+    int high = n - 1;
+
+    while (low <= high) {
+        int mid = low + ((high - low) >> 1);
+
+        if (a[mid] <= value) {
+            if((mid == n-1) || (a[mid+1] > value)) return mid;
+            else high = mid + 1;
+        }else {
+            high = mid - 1;
+        }
+    }
+}
+
+/**
+ * 二分法求平方根
+ * @param x
+ * @param precision
+ * @return
+ */
+double binary_sqrt(double x, double precision)
+{
+    if (x < 0) {
+        printf("Error: x is negative");
+    }
+    double low = 0;
+    double high = x;
+    if (x > 0 && x < 1) {
+        low = x;
+        high = 1;
+    }
+
+    double mid = low + (high - low) / 2;
+    while (high - low > precision) {
+        if (mid * mid > x) {
+            high = mid;
+        } else if (mid * mid < x) {
+            low = mid;
+        } else {
+            return mid;
+        }
+        mid = low + (high - low) / 2;
+    }
+    return mid;
+}
+
+
 
 /**
  * 查找函数benchmark
--- search/search.h
+++ search/search.h
@@ -3,6 +3,16 @@
 
 int binary_search(int a[], int n, int value);
 
+int binary_search_equal_first(int a[], int n, int value);
+
+int binary_search_equal_last(int a[], int n, int value);
+
+int binary_search_gt_first(int a[], int n, int value);
+
+int binary_search_lt_first(int a[], int n, int value);
+
+double binary_sqrt(double x, double precision);
+
 void generate_order_array(int a[], int len);
 
 double benchmark_search(int (*search_func) (int a[], int n, int value));
